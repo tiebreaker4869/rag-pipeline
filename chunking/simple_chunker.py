261bbc9c7ae6e4a7049650c6dd41ead3e7401384
Lifan Sun <lifansun1412@gmail.com>
2025-11-22 01:11:53 -0800

    feat: add simple chunker

---
+++ chunking/simple_chunker.py
@@ -0,0 +1,13 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing import List
+
+class SimpleChunker:
+    def __init__(self, chunk_size: int, chunk_overlap: int, separators=["\n\n", "\n", " ", ""]):
+        self.splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=separators,
+        )
+    def split_text(self, text: str) -> List[str]:
+        chunks = self.splitter.split_text(text)
+        return chunks
